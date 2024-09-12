e33d84b36ad55ba1cf420a76f85986c3d24b3064
Gabriel Guedes Ambrogi <g.guedes.a@hotmail.com>
2024-09-12 17:58:13 -0300

    feat: add product list and item components

---
+++ app/_components/product-list.tsx
@@ -0,0 +1,32 @@
+import { db } from "../_lib/prisma";
+import ProductItem from "./product-item";
+
+const ProductList = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    orderBy: {
+      discountPercentage: "desc",
+    },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+  return (
+    <div className="flex gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
+import ProductList from "./_components/product-list";
+import { Button } from "./_components/ui/button";
+import { ChevronRightIcon } from "lucide-react";
 
 const Home = () => {
   return (
@@ -22,6 +25,16 @@ const Home = () => {
           className="h-auto w-full object-contain"
           sizes="100vw"
         />
+      </div>
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos recomendados</h2>
+          <Button variant="link" className="h-fit p-0">
+            Ver todos
+            <ChevronRightIcon size={16} />
+          </Button>
+        </div>
+        <ProductList />
       </div>
     </>
   );
