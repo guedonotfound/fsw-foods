117bc257d40972fa9ca95e63fee6eacb5568de0f
Gabriel Guedes Ambrogi <g.guedes.a@hotmail.com>
2024-09-13 18:01:28 -0300

    refactor: componentize product image and return button on product page

---
+++ app/product/[id]/_component/product-image.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { Product } from "@prisma/client";
+import { ChevronLeftIcon } from "lucide-react";
+import Image from "next/image";
+
+interface ProductImageProps {
+  product: Pick<Product, "name" | "imageUrl">;
+}
+
+const ProductImage = ({ product }: ProductImageProps) => {
+  return (
+    <div className="relative h-[360px] w-full">
+      <Image
+        src={product?.imageUrl}
+        alt={product?.name}
+        fill
+        className="object-cover"
+      />
+      <Button
+        size="icon"
+        className="absolute left-4 top-4 rounded-full bg-white text-foreground hover:text-white"
+      >
+        <ChevronLeftIcon />
+      </Button>
+    </div>
+  );
+};
+
+export default ProductImage;
--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/app/_components/ui/button";
 import {
   calculateProductTotalPrice,
   formatCurrency,
 } from "@/app/_helpers/price";
 import { db } from "@/app/_lib/prisma";
-import { ArrowDownIcon, ChevronLeftIcon } from "lucide-react";
+import { ArrowDownIcon } from "lucide-react";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import ProductImage from "./_component/product-image";
 
 interface ProductPageProps {
   params: {
@@ -30,20 +30,7 @@ const ProductPage = async ({ params: { id } }: ProductPageProps) => {
 
   return (
     <div>
-      <div className="relative h-[360px] w-full">
-        <Image
-          src={product?.imageUrl}
-          alt={product?.name}
-          fill
-          className="object-cover"
-        />
-        <Button
-          size="icon"
-          className="absolute left-4 top-4 rounded-full bg-white text-foreground hover:text-white"
-        >
-          <ChevronLeftIcon />
-        </Button>
-      </div>
+      <ProductImage product={product} />
       <div className="p-5">
         <div className="flex items-center gap-[6px]">
           <div className="relative h-6 w-6 object-cover">
