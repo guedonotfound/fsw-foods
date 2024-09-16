9e1dfb6cdabf9e229f19f8598835113bd6ea729c
Gabriel Guedes Ambrogi <g.guedes.a@hotmail.com>
2024-09-16 18:03:10 -0300

    fix: change page to show "products" instead of "restaurants"

--- app/products/recommended/page.tsx
+++ app/products/recommended/page.tsx
@@ -20,9 +20,7 @@ const RecommendedProducts = async () => {
     <>
       <Header />
       <div className="px-5 py-6">
-        <h2 className="mb-6 text-lg font-semibold">
-          Restaurantes recomendados
-        </h2>
+        <h2 className="mb-6 text-lg font-semibold">Produtos recomendados</h2>
         <div className="grid grid-cols-2 gap-6">
           {products.map((product) => (
             <ProductItem
