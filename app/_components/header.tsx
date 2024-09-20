8d58575ce6dd923bd1b274f887e967365df8e9b1
Gabriel Guedes Ambrogi <g.guedes.a@hotmail.com>
2024-09-20 12:26:26 -0300

    feat: add initial menu sheet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,37 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import {
+  HeartIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+  ScrollTextIcon,
+} from "lucide-react";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { Separator } from "./ui/separator";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const router = useRouter();
+  const { data } = useSession();
+
+  const handleHomeClick = () => router.push("/");
+
+  const handleSightInClick = () => signIn();
+  const handleSightOutClick = () => signOut();
+
   return (
     <div className="flex justify-between px-5 pt-6">
       <Link href="/">
@@ -11,13 +39,83 @@ const Header = () => {
           <Image src="/logo.png" alt="FSW Foods" fill />
         </div>
       </Link>
-      <Button
-        size="icon"
-        variant="outline"
-        className="border-none bg-transparent"
-      >
-        <MenuIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger>
+          <Button
+            size="icon"
+            variant="outline"
+            className="border-none bg-transparent"
+          >
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+          {data?.user ? (
+            <div className="flex items-center gap-3 pt-6">
+              <Avatar>
+                <AvatarImage src={data.user.image as string | undefined} />
+              </Avatar>
+              <div>
+                <h3>{data.user.name}</h3>
+                <span className="block text-xs text-muted-foreground">
+                  {data.user.email}
+                </span>
+              </div>
+            </div>
+          ) : (
+            <div className="flex items-center justify-between pt-6">
+              <h2 className="font-semibold">Olá, faça seu login!</h2>
+              <Button size="icon" onClick={handleSightInClick}>
+                <LogInIcon />
+              </Button>
+            </div>
+          )}
+
+          <div className="py-6">
+            <Separator />
+          </div>
+          <div className="space-y-2">
+            <Button
+              className="w-full justify-start gap-3 rounded-full text-sm font-normal"
+              onClick={handleHomeClick}
+            >
+              <HomeIcon size={16} />
+              <span className="block">Início</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start gap-3 rounded-full text-sm font-normal"
+            >
+              <ScrollTextIcon size={16} />
+              <span className="block">Meus pedidos</span>
+            </Button>
+            <Button
+              variant="ghost"
+              className="w-full justify-start gap-3 rounded-full text-sm font-normal"
+            >
+              <HeartIcon size={16} />
+              <span className="block">Restaurantes favoritos</span>
+            </Button>
+          </div>
+          <div className="py-6">
+            <Separator />
+          </div>
+
+          {data?.user && (
+            <Button
+              variant="outline"
+              className="w-full justify-start gap-3 rounded-full text-sm font-normal"
+              onClick={handleSightOutClick}
+            >
+              <LogOutIcon size={16} />
+              <span>Sair da conta</span>
+            </Button>
+          )}
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
